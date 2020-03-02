c059c876a16022b6767483bf39a89c5daa878ab6
bohdan <bodyaalyas12@gmail.com>
2020-03-02 13:39:19 +0200

    Updates

---
+++ precache-manifest.9aa810c9c179a61917b8090c5749f242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a59e298e83f8e9694a25f39250e56e",
+    "url": "/user-list/index.html"
+  },
+  {
+    "revision": "77d40abb075f6447bd36",
+    "url": "/user-list/static/js/2.76dc1a3c.chunk.js"
+  },
+  {
+    "revision": "ad07d5a822c7d76ebe350a420e0c45bf",
+    "url": "/user-list/static/js/2.76dc1a3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37336490f36721c536b5",
+    "url": "/user-list/static/js/main.94f4252b.chunk.js"
+  },
+  {
+    "revision": "52a8c9f5507479bb58a0",
+    "url": "/user-list/static/js/runtime-main.cd421dcb.js"
+  },
+  {
+    "revision": "a67902d0a40893627b2aacc2b2e3c234",
+    "url": "/user-list/static/media/bg.a67902d0.jpg"
+  }
+]);
